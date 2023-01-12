372dcb6da9bb98ddab1ec5bd719f0aad933915e3
woodroww <example528@gmail.com>
2023-01-12 15:26:19 -0500

    updated to bevy 0.9.1

--- assets/my_material.wgsl
+++ assets/my_material.wgsl
@@ -1,4 +1,6 @@
-//https://github.com/bevyengine/bevy/blob/c2da7800e3671ad92e775529070a814d0bc2f5f8/crates/bevy_sprite/src/mesh2d/mesh2d.wgsl
+// for the globals.time
+#import bevy_sprite::mesh2d_view_bindings
+
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
     @location(0) world_position: vec4<f32>,
@@ -6,13 +8,13 @@ struct VertexOutput {
     @location(2) uv: vec2<f32>,
 };
 
-struct MyMat {
+struct CoolMaterial {
     color: vec4<f32>,
-    time: f32,
-};
+    health: f32,
+}
 
 @group(1) @binding(0)
-var<uniform> uniform_data: MyMat;
+var<uniform> material: CoolMaterial;
 @group(1) @binding(1)
 var texture: texture_2d<f32>;
 @group(1) @binding(2)
@@ -20,10 +22,7 @@ var our_sampler: sampler;
 
 @fragment
 fn fragment(input: VertexOutput) -> @location(0) vec4<f32> {
-    var output_color = vec4<f32>(uniform_data.time, 1.0, 1.0, 1.0);
+    var output_color = vec4<f32>(globals.time % 1.0, 1.0, 1.0, 1.0);
     output_color = output_color * textureSample(texture, our_sampler, input.uv);
-    output_color = output_color * uniform_data.color;
-    return output_color;
+    return output_color * material.color;
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-#![allow(clippy::redundant_field_names)]
-#![allow(clippy::type_complexity)]
-#![allow(clippy::too_many_arguments)]
-
 use bevy::{
     prelude::*,
     reflect::TypeUuid,
     render::{
         camera::ScalingMode,
         render_resource::{AsBindGroup, ShaderRef},
-        renderer::RenderQueue,
     },
-    render::{
-        extract_resource::{ExtractResource, ExtractResourcePlugin},
-        render_resource::*,
-        Extract, RenderApp, RenderStage,
-    },
-    sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle, RenderMaterials2d},
+    sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle},
     window::PresentMode,
 };
 
@@ -29,7 +19,7 @@ pub struct CoolMaterial {
     #[uniform(0)]
     color: Color,
     #[uniform(0)]
-    time: f32,
+    health: f32,
     #[texture(1)]
     #[sampler(2)]
     image: Handle<Image>,
@@ -41,39 +31,24 @@ impl Material2d for CoolMaterial {
     }
 }
 
-#[derive(Clone, ShaderType)]
-struct CoolMaterialUniformData {
-    color: Color,
-    time: f32,
-}
-
-#[derive(Component, Clone, Copy)]
-struct Health {
-    value: f32,
-}
-
 fn main() {
-    let mut app = App::new();
-    app.insert_resource(ClearColor(CLEAR))
-        .insert_resource(WindowDescriptor {
-            width: HEIGHT * RESOLUTION,
-            height: HEIGHT,
-            title: "Bevy Material Tutorial".to_string(),
-            present_mode: PresentMode::Fifo,
-            resizable: false,
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: HEIGHT * RESOLUTION,
+                height: HEIGHT,
+                title: "Bevy Material Tutorial".to_string(),
+                present_mode: PresentMode::Fifo,
+                resizable: false,
+                ..Default::default()
+            },
             ..Default::default()
-        })
-        .add_plugins(DefaultPlugins)
+        }))
+        .insert_resource(ClearColor(CLEAR))
         .add_plugin(Material2dPlugin::<CoolMaterial>::default())
         .add_startup_system(spawn_camera)
-        .add_plugin(ExtractResourcePlugin::<ExtractedTime>::default())
-        .add_startup_system(setup);
-    // Add all render world systems/resources
-    app.sub_app_mut(RenderApp)
-        .add_system_to_stage(RenderStage::Extract, extract_health)
-        .add_system_to_stage(RenderStage::Prepare, prepare_my_material);
-
-    app.run();
+        .add_startup_system(setup)
+        .run();
 }
 
 fn setup(
@@ -82,91 +57,34 @@ fn setup(
     mut my_material_assets: ResMut<Assets<CoolMaterial>>,
     assets: Res<AssetServer>,
 ) {
-    commands
-        .spawn_bundle(MaterialMesh2dBundle {
-            mesh: mesh_assets.add(Mesh::from(shape::Quad::default())).into(),
-            material: my_material_assets.add(CoolMaterial {
-                color: Color::rgb(0.0, 1.0, 0.3),
-                time: 0.0,
-                image: assets.load("awesome.png"),
-            }),
-            transform: Transform::from_xyz(-0.6, 0.0, 0.0),
-            ..default()
-        })
-        .insert(Health { value: 0.2 });
-    commands
-        .spawn_bundle(MaterialMesh2dBundle {
-            mesh: mesh_assets.add(Mesh::from(shape::Quad::default())).into(),
-            material: my_material_assets.add(CoolMaterial {
-                color: Color::rgb(0.0, 1.0, 0.3),
-                time: 0.0,
-                image: assets.load("awesome.png"),
-            }),
-            transform: Transform::from_xyz(0.6, 0.0, 0.0),
-            ..default()
-        })
-        .insert(Health { value: 0.8 });
-}
-
-struct ExtractedTime {
-    seconds_since_startup: f32,
-}
-
-impl ExtractResource for ExtractedTime {
-    type Source = Time;
-
-    fn extract_resource(time: &Self::Source) -> Self {
-        ExtractedTime {
-            seconds_since_startup: time.seconds_since_startup() as f32,
-        }
-    }
-}
-
-fn extract_health(
-    mut commands: Commands,
-    health_query: Extract<Query<(Entity, &Health, &Handle<CoolMaterial>)>>,
-) {
-    for (entity, health, handle) in health_query.iter() {
-        commands
-            .get_or_spawn(entity)
-            .insert(*health)
-            .insert(handle.clone());
-    }
-}
-
-fn prepare_my_material(
-    materials: Res<RenderMaterials2d<CoolMaterial>>,
-    health_query: Query<(&Health, &Handle<CoolMaterial>)>,
-    time: Res<ExtractedTime>,
-    render_queue: Res<RenderQueue>,
-) {
-    for (health, handle) in health_query.iter() {
-        if let Some(material) = materials.get(handle) {
-            let binding = &material.bindings[2];
-            if let OwnedBindingResource::Buffer(cur_buffer) = binding {
-                let mut buffer = encase::UniformBuffer::new(Vec::new());
-                buffer
-                    .write(&CoolMaterialUniformData {
-                        color: Color::rgb(health.value, 0.0, 0.0),
-                        time: time.seconds_since_startup % 1.0,
-                    })
-                    .unwrap();
-                render_queue.write_buffer(cur_buffer, 0, buffer.as_ref());
-            }
-        }
-    }
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: mesh_assets.add(Mesh::from(shape::Quad::default())).into(),
+        material: my_material_assets.add(CoolMaterial {
+            color: Color::rgb(0.2, 0.0, 0.0),
+            image: assets.load("awesome.png"),
+            health: 0.2,
+        }),
+        transform: Transform::from_xyz(-0.6, 0.0, 0.0),
+        ..default()
+    });
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: mesh_assets.add(Mesh::from(shape::Quad::default())).into(),
+        material: my_material_assets.add(CoolMaterial {
+            color: Color::rgb(0.8, 0.0, 0.0),
+            image: assets.load("awesome.png"),
+            health: 0.8,
+        }),
+        transform: Transform::from_xyz(0.6, 0.0, 0.0),
+        ..default()
+    });
 }
 
 fn spawn_camera(mut commands: Commands) {
     let mut camera = Camera2dBundle::default();
-
     camera.projection.right = 1.0 * RESOLUTION;
     camera.projection.left = -1.0 * RESOLUTION;
-
     camera.projection.top = 1.0;
     camera.projection.bottom = -1.0;
-
     camera.projection.scaling_mode = ScalingMode::None;
-
-    commands.spawn_bundle(camera);
+    commands.spawn(camera);
 }
